491a435390214a011ef49991ea9ae270c88b3fa0
Thai Villaluna <thaivillaluna@gmail.com>
2024-02-17 16:41:47 -0800

    setup customizable search terms

--- sam_api/main.py
+++ sam_api/main.py
@@ -1,23 +1,52 @@
 import requests
 import os
 import json
+import datetime
 
-def init_search():
+def init_search_terms(limit=10, posted_from="01/01/2024",
+                      posted_to=None, ptype=None, ncode=None):
     api_key = os.getenv('SAM_API_KEY')
+    print(api_key)
     if not api_key:
         print("Valid API Key not found")
         exit(-1)
     
-    
+    if not posted_to:
+        posted_to = datetime.date.today().strftime("%m/%d/%y") 
+
     terms = {}
+    terms['limit'] = limit
+    terms['postedFrom'] = posted_from
+    terms['postedTo'] = posted_to
     terms['api_key'] = api_key
+    terms['ncode'] = ncode
+    terms['pcode'] = ptype
+    
+    
+    return terms
+
+
+def init_search(terms):
+    search = f"https://api.sam.gov/prod/opportunities/v2/search?api_key={terms.get('api_key')}"
+
+    for key, value in terms.items():
+        if(key == 'api_key'):
+            continue
+        if not terms.get(key):
+            continue
         
-    search = f"https://api.sam.gov/prod/opportunities/v2/search?limit=5&api_key={terms.get('api_key')}&postedFrom=01/01/2024&postedTo=05/10/2024&ptype=a&deptname=general"
+        if isinstance(terms.get(key), list):
+            for search_term in value:
+                search += f"&{key}={search_term}"
+        else:
+            search += f"&{key}={value}"
+    
     return search
 
 if __name__=="__main__":
     
-    search = init_search()
+    terms = init_search_terms(ncode=541330, ptype=['r', 'o', 's', 'k'])
+    search = init_search(terms)
     
     response = requests.get(search)
     results_json = response.json()
@@ -27,7 +56,7 @@ if __name__=="__main__":
         print("Exiting with error.")
         exit(1)
         
-    # sets opp_list to a list of dictionaries, one for each opp
+    # sets opp_list to a list of dictionaries, one for each opportunity
     opp_list = results_json['opportunitiesData']
         
     for opp_dict in opp_list:
